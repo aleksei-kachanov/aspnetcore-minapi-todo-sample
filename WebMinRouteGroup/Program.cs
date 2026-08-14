3557f62c658f57f64cf999b80596273b6e5675fe
Aleksei Kachanov <aleksei_kachanov@epam.com>
2026-08-14 13:32:11 -0500

    Revert "fix: await db.Database.MigrateAsync() on startup (MVP-001)"
    This reverts commit aca316fd523a816e77c274ef7a5f043c9c89f8c6.

--- WebMinRouteGroup/Program.cs
+++ WebMinRouteGroup/Program.cs
@@ -19,7 +19,7 @@ var app = builder.Build();
 
 using var scope = app.Services.CreateScope();
 var db = scope.ServiceProvider.GetService<TodoGroupDbContext>();
-if (db != null) await db.Database.MigrateAsync();
+db?.Database.MigrateAsync();
 
 // todoV1 endpoints
 app.MapGroup("/todos/v1")
