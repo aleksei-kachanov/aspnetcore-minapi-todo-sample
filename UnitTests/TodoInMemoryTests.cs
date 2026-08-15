ee69d697eaf0141a4a1fbd70ceebf0becbb13b6b
Aleksei Kachanov <aleksei_kachanov@epam.com>
2026-08-14 20:36:03 -0500

    fix: update unit tests for UpdateTodo signature change (UpdateTodoDto + id param)

--- UnitTests/TodoInMemoryTests.cs
+++ UnitTests/TodoInMemoryTests.cs
@@ -151,15 +151,14 @@ public class TodoInMemoryTests
 
         await context.SaveChangesAsync();
 
-        var updatedTodo = new Todo
+        var updatedTodo = new UpdateTodoDto
         {
-            Id = 1,
             Title = "Updated test title",
             IsDone = true
         };
 
         //Act
-        var result = await TodoEndpointsV1.UpdateTodo(updatedTodo, context);
+        var result = await TodoEndpointsV1.UpdateTodo(1, updatedTodo, context);
 
         //Assert
         Assert.IsType<Results<Created<Todo>, NotFound>>(result);
